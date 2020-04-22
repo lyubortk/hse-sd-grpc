3648910af08d763b131aaeb8b4ef129d96d346c0
Konstantin Lyubort <lyubortk@gmail.com>
2020-04-22 17:15:09 +0300

    add simple grpc server and client

--- src/main/java/ru/hse/grpc/Main.java
+++ src/main/java/ru/hse/grpc/Main.java
@@ -1,7 +1,25 @@
 package ru.hse.grpc;
 
 public class Main {
+    public static void printUsage() {
+        System.err.println("Usage: ./gradlew run "); //TODO
+    }
+
     public static void main(String[] args) {
-        Model.ChatMessage msg;
+        if (args.length < 2) {
+            printUsage();
+        }
+
+        try {
+            if (args.length == 2) {
+                ChatServer.run(args[0], Integer.parseInt(args[1]));
+            } else if (args.length == 3) {
+                ChatClient.run(args[0], args[1], Integer.parseInt(args[2]));
+            } else {
+                printUsage();
+            }
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
